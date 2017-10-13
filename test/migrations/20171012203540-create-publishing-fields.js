618839bfd2fa48291c1308fee45455eefac3b88c
kyleschaeffer <kyle@oldrivercreative.com>
2017-10-13 10:56:39 -0400

    Add taxonomy field tests, update publishing column tests

--- test/migrations/20171012203540-create-publishing-fields.js
+++ test/migrations/20171012203540-create-publishing-fields.js
@@ -6,13 +6,68 @@ module.exports = {
    */
   up(engineer) {
     // HTML field
-    engineer.field.fromSchema('<Field Name="TestHTMLField" StaticName="TestHTMLField" SourceID="http://schemas.microsoft.com/sharepoint/v3" Group="_Test Site Fields" DisplayName="TestHTMLField" Description="A test HTML field" Type="HTML" Required="FALSE" RichText="TRUE" RichTextMode="FullHtml" />');
+    engineer.field.fromSchema(`
+      <Field
+        Type="HTML"
+        Name="TestHTMLField"
+        StaticName="TestHTMLField"
+        DisplayName="TestHTMLField"
+        Description="A test HTML field"
+        Group="_Test Site Fields"
+        RichText="TRUE"
+        RichTextMode="FullHtml"
+        Required="FALSE"
+        SourceID="http://schemas.microsoft.com/sharepoint/v3">
+      </Field>
+    `);
 
-    // Publishing image field
-    engineer.field.fromSchema('<Field Name="TestPublishingImageField" StaticName="TestPublishingImageField" SourceID="http://schemas.microsoft.com/sharepoint/v3" Group="_Test Site Fields" DisplayName="TestPublishingImageField" Description="A test publishing image field" Type="Image" Required="FALSE" RichText="TRUE" RichTextMode="FullHtml" />');
+    // Image field
+    engineer.field.fromSchema(`
+      <Field
+        Type="Image"
+        Name="TestImageField"
+        StaticName="TestImageField"
+        DisplayName="TestImageField"
+        Description="A test Image field"
+        Group="_Test Site Fields"
+        RichText="TRUE"
+        RichTextMode="FullHtml"
+        Required="FALSE"
+        SourceID="http://schemas.microsoft.com/sharepoint/v3">
+      </Field>
+    `);
 
-    // Summary links field
-    engineer.field.fromSchema('<Field Name="TestSummaryLinksField" StaticName="TestSummaryLinksField" SourceID="http://schemas.microsoft.com/sharepoint/v3" Group="_Test Site Fields" DisplayName="TestSummaryLinksField" Description="A test summary links field" Type="SummaryLinks" Required="FALSE" RichText="TRUE" RichTextMode="FullHtml" />');
+    // Link field
+    engineer.field.fromSchema(`
+      <Field
+        Type="Link"
+        Name="TestLinkField"
+        StaticName="TestLinkField"
+        DisplayName="TestLinkField"
+        Description="A test Link field"
+        Group="_Test Site Fields"
+        RichText="TRUE"
+        RichTextMode="ThemeHtml"
+        Required="FALSE"
+        SourceID="http://schemas.microsoft.com/sharepoint/v3">
+      </Field>
+    `);
+
+    // SummaryLinks field
+    engineer.field.fromSchema(`
+      <Field
+        Type="SummaryLinks"
+        Name="TestSummaryLinksField"
+        StaticName="TestSummaryLinksField"
+        DisplayName="TestSummaryLinksField"
+        Description="A test SummaryLinks field"
+        Group="_Test Site Fields"
+        RichText="TRUE"
+        RichTextMode="FullHtml"
+        Required="FALSE"
+        SourceID="http://schemas.microsoft.com/sharepoint/v3">
+      </Field>
+    `);
   },
 
   /**
@@ -23,7 +78,8 @@ module.exports = {
   down(engineer) {
     // Delete fields
     engineer.field.delete('TestHTMLField');
-    engineer.field.delete('TestPublishingImageField');
+    engineer.field.delete('TestImageField');
+    engineer.field.delete('TestLinkField');
     engineer.field.delete('TestSummaryLinksField');
   },
 };
