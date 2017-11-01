f36edf7a6ba49a35fcdd4d51606ba1d66adabd2c
kyleschaeffer <kyle@oldrivercreative.com>
2017-11-01 11:23:21 -0400

    Browser testing and updates for IE

--- js/app.js
+++ js/app.js
@@ -5,14 +5,14 @@ if (activeMenuItem) {
   if (headings.length) {
     var ul = document.createElement('ul');
     ul.classList.add('jump-nav');
-    headings.forEach((heading) => {
+    for (var i = 0; i < headings.length; i++) {
       var li = document.createElement('li');
       var a = document.createElement('a');
-      a.setAttribute('href', '#' + heading.getAttribute('id'));
-      a.textContent = heading.getAttribute('data-jump');
+      a.setAttribute('href', '#' + headings[i].getAttribute('id'));
+      a.textContent = headings[i].getAttribute('data-jump');
       li.appendChild(a);
       ul.appendChild(li);
-    });
+    }
     activeMenuItem.appendChild(ul);
   }
 }
