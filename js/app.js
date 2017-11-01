565efc05e9db882758d2c7da7841aac1333bf003
kyleschaeffer <kyle@oldrivercreative.com>
2017-11-01 16:29:37 -0400

    Add intro video

--- js/app.js
+++ js/app.js
@@ -16,3 +16,68 @@ if (activeMenuItem) {
     activeMenuItem.appendChild(ul);
   }
 }
+
+// Video controls
+var player = {
+  // Elements
+  stage: document.querySelector('.stage'),
+  video: document.querySelector('video'),
+  playBtn: document.querySelector('.play'),
+  againBtn: document.querySelector('.play-again'),
+
+  // Clear the stage
+  clear: function() {
+    player.stage.classList.remove('lobby');
+    player.stage.classList.remove('curtains');
+  },
+
+  // Put stage in lobby mode
+  lobby: function() {
+    player.stage.classList.add('lobby');
+  },
+
+  // Put stage in curtains mode
+  curtains: function() {
+    player.stage.classList.add('curtains');
+  },
+
+  // Play video
+  play: function() {
+    player.clear();
+    player.video.play();
+    player.playBtn.innerHTML = '<i class="fa fa-pause" aria-hidden="true"></i>';
+    player.playBtn.classList.add('paused');
+  },
+
+  // Pause video
+  pause: function() {
+    player.video.pause();
+    player.playBtn.innerHTML = '<i class="fa fa-play" aria-hidden="true"></i>';
+    player.playBtn.classList.remove('paused');
+  },
+
+  // Toggle play/pause
+  toggle: function() {
+    if (player.video.paused) player.play();
+    else player.pause();
+  },
+
+  // Restart video
+  restart: function() {
+    player.clear();
+    player.video.currentTime = 0;
+    if (player.video.paused) player.play();
+  },
+
+  // Init player
+  init: function() {
+    if (player.stage) player.lobby();
+    if (player.playBtn) player.playBtn.addEventListener('click', player.toggle);
+    if (player.againBtn) player.againBtn.addEventListener('click', player.restart);
+    if (player.video) {
+      player.video.addEventListener('ended', player.curtains);
+      player.video.addEventListener('click', player.toggle);
+    }
+  },
+};
+player.init();
